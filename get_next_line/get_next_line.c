185f52fe5dc948b2cd7b43c08e1a3f73fefe4fdd
bentowski <benjamin.baudry@hotmail.com>
2019-11-12 22:11:00 +0100

    maj gnl

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -14,6 +14,49 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	return (0);
 }
 
+size_t	ft_strlen(const char *s)
+{
+	size_t x;
+
+	x = 0;
+	while (*s)
+	{
+		x++;
+		s++;
+	}
+	return (x);
+}
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	char *psrc;
+	char *pdst;
+
+	psrc = (char *)src;
+	pdst = (char *)dst;
+	if (!src && !dst)
+		return (NULL);
+	while (n > 0)
+	{
+		pdst[n - 1] = psrc[n - 1];
+		n--;
+	}
+	return (dst);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	size_t	len;
+	char	*cpy;
+
+	len = ft_strlen(s1) + 1;
+	if (!(cpy = (char *)malloc(len)))
+		return (NULL);
+	ft_memcpy(cpy, s1, len);
+	return (cpy);
+}
+
+
 char *ft_realloc(char *s, char add)
 {
   int x;
@@ -21,15 +64,15 @@ char *ft_realloc(char *s, char add)
   char *new;
 
   x = 0;
+	i = 0;
   while (s[x] != '\0')
     x++;
-	printf("%d\n", x);
   if (!((new = (char*)malloc(x + 2))))
     return (NULL);
   while (x >= i)
   {
     new[i] = s[i];
-    i--;
+    i++;
   }
 	new[i + 1] = add;
 	new[i + 2] = '\0';
@@ -44,12 +87,11 @@ int get_next_line(int fd, char **line)
 
 
   i = 0;
-  if (!(s = (char*)malloc(1)))
+  if (!(s = (char*)malloc(2)))
     return (-1);
 	s[1] = '\0';
   if (!(read(fd, &courant, 1)))
     return (-1);
-	s[i] = courant;
   while (courant != '\n' && courant != '\0')
   {
     s = ft_realloc(s, courant);
@@ -58,7 +100,8 @@ int get_next_line(int fd, char **line)
 		s[i] = '\0';
     if(!(read(fd, &courant, 1)))
       return (-1);
-    // printf("%s", s);
   }
+	*line = ft_strdup(s);
+	printf("%s\n", *line);
   return (1);
 }
--- get_next_line/main.c
+++ get_next_line/main.c
@@ -10,16 +10,7 @@ int main()
 
   essai = "e";
   test = &essai;
-  filename = "text";
+  filename = "/dev/random";
   fd = open(filename, O_RDONLY);
-  // write(1, "1 ", 2);
-  get_next_line(fd, test);
-  // write(1, "\n2 ", 3);
-  get_next_line(fd, test);
-  // write(1, "\n3 ", 3);
-  get_next_line(fd, test);
-  // write(1, "\n4 ", 3);
-  get_next_line(fd, test);
-  // write(1, "\n5 ", 3);
-  get_next_line(fd, test);
+  while (get_next_line(fd, test));
 }
