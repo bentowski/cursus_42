f90365f909e2d64c9ed1fb75f31e0c1836ac2270
bentowski <benjamin.baudry@hotmail.com>
2020-01-10 23:40:54 +0100

    probleme au dela de 7

--- sandbox/main.c
+++ sandbox/main.c
@@ -4,28 +4,29 @@
 int main()
 {
   const char *filename1;
-  // const char *filename2;
-  // const char *filename3;
+  const char *filename2;
+  const char *filename3;
   int fd1;
-  // int fd2;
-  // int fd3;
+  int fd2;
+  int fd3;
   char **test;
   char *essai;
 
   essai = "";
   test = &essai;
   filename1 = "text";
-  // filename2 = "text2";
-  // filename3 = "text3";
+  filename2 = "text2";
+  filename3 = "text3";
   fd1 = open(filename1, O_RDONLY);
-  // fd2 = open(filename2, O_RDONLY);
-  // fd3 = open(filename3, O_RDONLY);
+  fd2 = open(filename2, O_RDONLY);
+  fd3 = open(filename3, O_RDONLY);
   get_next_line(fd1, test);
   get_next_line(fd1, test);
-  get_next_line(fd1, test);
-  get_next_line(fd1, test);
-  get_next_line(fd1, test);
+  get_next_line(fd2, test);
+  get_next_line(fd3, test);
+  get_next_line(fd2, test);
+  get_next_line(fd3, test);
   write(1, "end", 3);
 
-  // system("leaks a.out");
+  system("leaks a.out");
 }
