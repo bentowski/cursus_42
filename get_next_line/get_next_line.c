d3413b32dd5eb496a2f1815b71f559766c2f1dcf
bentowski <benjamin.baudry@hotmail.com>
2020-03-02 16:13:58 +0100

    pb return -1 et fct 26ln

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,129 +1,143 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbaudry <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/02 16:00:50 by bbaudry           #+#    #+#             */
+/*   Updated: 2020/03/02 16:11:31 by bbaudry          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-static char	*ft_strdup(char *s1)
+static	char	*ft_strdup(char *s1)
 {
-  int	  len;
-  char *cpy;
-  int   x;
+	int		len;
+	char	*cpy;
+	int		x;
 
-  x = -1;
-  len = 0;
-  if (!s1)
-    return (NULL);
-  while (s1[len++]);
-  if (!(cpy = (char *)malloc(len + 1)))
-    return (NULL);
-  while(++x < len)
-    cpy[x]= s1[x];
+	x = -1;
+	len = 0;
+	if (!s1)
+		return (NULL);
+	while (s1[len])
+	{
+		len++;
+	}
+	if (!(cpy = (char *)malloc(len + 1)))
+		return (NULL);
+	while (++x < len)
+		cpy[x] = s1[x];
 	return (cpy);
 }
 
-static int ft_readdeux(int fd, char **toreturn, char ***saved)
+static	int		ft_readdeux(int fd, char **toreturn, char ***saved)
 {
-  int ret;
-  char courant[BUFFER_SIZE + 1];
-  int i;
+	int		ret;
+	char	courant[BUFFER_SIZE + 1];
+	int		i;
 
-  i = 0;
-  courant[BUFFER_SIZE] = '\0';
-  while (1)
-  {
-    i = 0;
-    if ((ret = read(fd, courant, BUFFER_SIZE)) <= 0)
-      break;
-    if (!(*toreturn = ft_strjoin(*toreturn, courant, &i)))
-      return (-1);
-    if (courant[i] == '\n')
-      break;
-  }
-  if (ret < 0)
-    return (-1);
-  if (courant[i + 1] != '\0')
-    if (!(**saved = ft_strdup(&courant[i + 1])))
-      return (-1);
-  if (ret == 0)
-    return (0);
-  return (1);
+	i = 0;
+	courant[BUFFER_SIZE] = '\0';
+	while (1)
+	{
+		i = 0;
+		if ((ret = read(fd, courant, BUFFER_SIZE)) <= 0)
+			break ;
+		if (!(*toreturn = ft_strjoin(*toreturn, courant, &i)))
+			return (-1);
+		if (courant[i] == '\n')
+			break ;
+	}
+	if (ret < 0)
+		return (-1);
+	if (courant[i + 1] != '\0')
+		if (!(**saved = ft_strdup(&courant[i + 1])))
+			return (-1);
+	if (ret == 0)
+		return (0);
+	return (1);
 }
 
-static int ft_read(int fd, char ***line, char **saved)
+static	int		ft_read(int fd, char ***line, char **saved)
 {
-  char *toreturn;
-  int ret;
+	char	*toreturn;
+	int		ret;
 
-  if (*saved)
-  {
-    if (!(toreturn = ft_strdup(*saved)))
-      return (-1);
-    free(*saved);
-    *saved = NULL;
-  }
-  else if (!(toreturn = (char *)malloc(1)))
-    return (-1);
-  if ((ret = ft_readdeux(fd, &toreturn, &saved)) <= 0)
-  {
-    free(toreturn);
-    return (ret);
-  }
-  if (!(**line = ft_strdup(toreturn)))
-    return (-1);
-  free(toreturn);
-  return (ret);
+	if (*saved)
+	{
+		if (!(toreturn = ft_strdup(*saved)))
+			return (-1);
+		free(*saved);
+		*saved = NULL;
+	}
+	else if (!(toreturn = (char *)malloc(1)))
+		return (-1);
+	if ((ret = ft_readdeux(fd, &toreturn, &saved)) <= 0)
+	{
+		free(toreturn);
+		return (ret);
+	}
+	if (!(**line = ft_strdup(toreturn)))
+		return (-1);
+	free(toreturn);
+	return (ret);
 }
 
-
-static int ft_noread(char **saved, char ***line)
+static	int		ft_noread(char **saved, char ***line)
 {
-  char *toreturn;
-  char *tmp;
-  int len;
-  int x;
+	char	*toreturn;
+	char	*tmp;
+	int		len;
+	int		x;
 
-  len = -1;
-  x = -1;
-  if (!(tmp = ft_strdup(*saved)))
-    return (-1);
-  free(*saved);
-  *saved = NULL;
-  while (tmp[++len] != '\0')
-    if (tmp[len] == '\n')
-      break;
-  if (!(toreturn = (char *)malloc(len + 1)))
-    return (-1);
-  toreturn[len] = '\0';
-  while (len - 1 > x++)
-    toreturn[x] = tmp[x];
-  if (!(**line = ft_strdup(toreturn)))
-    return (-1);
-  free(toreturn);
-  if (!(*saved = ft_strdup(&tmp[len + 1])))
-    return (-1);
-  free(tmp);
-  return (1);
+	len = -1;
+	x = -1;
+	if (!(tmp = ft_strdup(*saved)))
+		return (-1);
+	free(*saved);
+	*saved = NULL;
+	while (tmp[++len] != '\0')
+		if (tmp[len] == '\n')
+			break ;
+	if (!(toreturn = (char *)malloc(len + 1)))
+		return (-1);
+	toreturn[len] = '\0';
+	while (len - 1 > x++)
+		toreturn[x] = tmp[x];
+	if (!(**line = ft_strdup(toreturn)))
+		return (-1);
+	free(toreturn);
+	if (!(*saved = ft_strdup(&tmp[len + 1])))
+		return (-1);
+	free(tmp);
+	return (1);
 }
 
-int get_next_line(int fd, char **line)
+int				get_next_line(int fd, char **line)
 {
-  static char *global[255];
-  int ret;
-  int ok;
-  int x;
+	static	char	*global[255];
+	int				ret;
+	int				ok;
+	int				x;
 
-  ok = 0;
-  x = 0;
-  if (fd >= 0)
-  {
-    if (global[fd])
-    {
-      while (global[fd][++x])
-        if (global[fd][x] == '\n')
-          ok = 1;
-    }
-    if (ok == 1)
-      ret = ft_noread(&global[fd], &line);
-    else
-      ret = ft_read(fd, &line, &global[fd]);
-    return (ret);
-  }
-  return (-1);
+	ok = 0;
+	x = 0;
+	if (fd >= 0)
+	{
+		if (global[fd])
+		{
+			while (global[fd][++x])
+				if (global[fd][x] == '\n')
+					ok = 1;
+		}
+		if (ok == 1)
+			ret = ft_noread(&global[fd], &line);
+		else
+			ret = ft_read(fd, &line, &global[fd]);
+		return (ret);
+	}
+	return (-1);
 }
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbaudry <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/02 16:00:22 by bbaudry           #+#    #+#             */
+/*   Updated: 2020/03/02 16:00:24 by bbaudry          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
 
@@ -5,29 +17,29 @@ char	*ft_strjoin(char *s1, char const *s2, int *i)
 {
 	char	*d;
 	int		len;
-  int   len2;
+	int   len2;
 	int		y;
-  int   x;
+	int   x;
 
 	y = 0;
-  x = 0;
-  len = 0;
-  len2 = 0;
+	x = 0;
+	len = 0;
+	len2 = 0;
 	if (!s2)
 		return (NULL);
-  while (s1[len++]);
-  while (s2[len2] && s2[len2] != '\n')
-    len2++;
+	while (s1[len++]);
+	while (s2[len2] && s2[len2] != '\n')
+		len2++;
 	len += len2 + 1;
 	if (!(d = (char *)malloc(len)))
 		return (NULL);
 	while (s1[x])
 		d[y++] = s1[x++];
-  x = 0;
+	x = 0;
 	while (s2[x] && s2[x] != '\n')
 		d[y++] = s2[x++];
 	d[y] = '\0';
-  *i = x;
-  free(s1);
+	*i = x;
+	free(s1);
 	return (d);
 }
