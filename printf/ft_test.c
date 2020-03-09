6eaa5550d87a2b329cbfbb55d6155a40e4ceebae
bentowski <benjamin.baudry@hotmail.com>
2020-03-09 17:40:14 +0100

    push gnl N1

--- printf/ft_test.c
+++ printf/ft_test.c
@@ -4,11 +4,18 @@
 int main()
 {
   int precision = 8;
-  int biggerPrecision = 19;
+  int biggerPrecision = 16;
   const char *greetings = "Hello world";
 
-  printf("|%.8s|\n", greetings);
-  printf("|%.*s|\n", precision , greetings);
-  printf("|%%|\n", precision, biggerPrecision);
-  printf("|%.*s|\n", biggerPrecision , greetings);
+  printf("1 : |%-.8s|\n", greetings);
+  printf("2 : |%.8s|\n", greetings);
+  printf("3 : |%8s|\n", greetings);
+  printf("4 : |%.s|\n", greetings);
+  printf("5 : |%-s|\n", greetings);
+  printf("6 : |%-.s|\n", greetings);
+  printf("7 : |%-.*s|\n", precision, greetings);
+  printf("8 : |%*s|\n", biggerPrecision, greetings);
+  printf("9 : |%.*s|\n", biggerPrecision , greetings);
+  printf("10 : |%-.*s|\n", biggerPrecision, greetings);
+  printf("11 : |%-*s|\n", biggerPrecision , greetings);
 }
