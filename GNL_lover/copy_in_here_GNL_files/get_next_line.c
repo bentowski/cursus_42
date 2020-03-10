635cbdf47bc800f00ad1c52f946571b02bc0836e
bentowski <benjamin.baudry@hotmail.com>
2020-03-10 20:27:48 +0100

    push gnl rate

---
+++ GNL_lover/copy_in_here_GNL_files/get_next_line.c
@@ -0,0 +1,145 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbaudry <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/02 16:00:50 by bbaudry           #+#    #+#             */
+/*   Updated: 2020/03/09 17:22:53 by bbaudry          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static	char	*ft_strdup(char *s1)
+{
+	int		len;
+	char	*cpy;
+	int		x;
+
+	x = -1;
+	len = 0;
+	if (!s1)
+		return (NULL);
+	while (s1[len])
+	{
+		len++;
+	}
+	if (!(cpy = (char *)malloc(len + 1)))
+		return (NULL);
+	while (++x < len)
+		cpy[x] = s1[x];
+	cpy[x] = '\0';
+	return (cpy);
+}
+
+static	int		ft_readdeux(int fd, char **toreturn, char ***saved)
+{
+	int		ret;
+	char	courant[BUFFER_SIZE + 1];
+	int		i;
+
+	i = 0;
+	courant[BUFFER_SIZE] = '\0';
+	while (1)
+	{
+		i = 0;
+		if ((ret = read(fd, courant, BUFFER_SIZE)) <= 0)
+			break ;
+		if (!(*toreturn = ft_strjoin(*toreturn, courant, &i)))
+			return (-1);
+		if (courant[i] == '\n' || courant[i] == '\0')
+			break ;
+	}
+	if (ret < 0)
+		return (-1);
+	if (courant[i + 1] != '\0')
+		if (!(**saved = ft_strdup(&courant[i + 1])))
+			return (-1);
+	return (ret);
+}
+
+static	int		ft_read(int fd, char ***line, char **saved)
+{
+	char	*toreturn;
+	int		ret;
+
+	if (*saved)
+	{
+		if (!(toreturn = ft_strdup(*saved)))
+			return (-1);
+		free(*saved);
+		*saved = NULL;
+	}
+	else
+	{
+		if (!(toreturn = (char *)malloc(1)))
+			return (-1);
+		toreturn[0] = '\0';
+	}
+	if ((ret = ft_readdeux(fd, &toreturn, &saved)) < 0)
+	{
+		free(toreturn);
+		return (ret);
+	}
+	if (!(**line = ft_strdup(toreturn)))
+		return (-1);
+	free(toreturn);
+	return (ret);
+}
+
+static	int		ft_noread(char **saved, char ***line)
+{
+	char	*toreturn;
+	char	*tmp;
+	int		len;
+	int		x;
+
+	len = 0;
+	x = -1;
+	if (!(tmp = ft_strdup(*saved)))
+		return (-1);
+	free(*saved);
+	*saved = NULL;
+	while (tmp[len] != '\0' && tmp[len] != '\n')
+		len++;
+	if (!(toreturn = (char *)malloc(len + 1)))
+		return (-1);
+	toreturn[len] = '\0';
+	while (len - 1 > x++)
+		toreturn[x] = tmp[x];
+	if (!(**line = ft_strdup(toreturn)))
+		return (-1);
+	free(toreturn);
+	if (!(*saved = ft_strdup(&tmp[len + 1])))
+		return (-1);
+	free(tmp);
+	return (1);
+}
+
+int				get_next_line(int fd, char **line)
+{
+	static	char	*saved[255];
+	int				ret;
+	int				ok;
+	int				x;
+
+	ok = 0;
+	x = -1;
+	if (fd >= 0)
+	{
+		if (saved[fd])
+		{
+			while (saved[fd][++x])
+				if (saved[fd][x] == '\n')
+					ok = 1;
+		}
+		if (ok == 1)
+			ret = ft_noread(&saved[fd], &line);
+		else
+			ret = ft_read(fd, &line, &saved[fd]);
+		return (ret);
+	}
+	return (-1);
+}
--- printf/ft_test.c
+++ printf/ft_test.c
@@ -5,17 +5,42 @@ int main()
 {
   int precision = 8;
   int biggerPrecision = 16;
+  int enteri = -17;
   const char *greetings = "Hello world";
 
-  printf("1 : |%-.8s|\n", greetings);
-  printf("2 : |%.8s|\n", greetings);
-  printf("3 : |%8s|\n", greetings);
-  printf("4 : |%.s|\n", greetings);
-  printf("5 : |%-s|\n", greetings);
-  printf("6 : |%-.s|\n", greetings);
-  printf("7 : |%-.*s|\n", precision, greetings);
-  printf("8 : |%*s|\n", biggerPrecision, greetings);
-  printf("9 : |%.*s|\n", biggerPrecision , greetings);
-  printf("10 : |%-.*s|\n", biggerPrecision, greetings);
-  printf("11 : |%-*s|\n", biggerPrecision , greetings);
+  printf("s 1 : |%-.8s|\n", greetings);
+  printf("s 2 : |%.8s|\n", greetings);
+  printf("s 3 : |%8s|\n", greetings);
+  printf("s 4 : |%.s|\n", greetings);
+  printf("s 5 : |%-s|\n", greetings);
+  printf("s 6 : |%-.s|\n", greetings);
+  printf("s 7 : |%-.*s|\n", precision, greetings);
+  printf("s 8 : |%*s|\n", biggerPrecision, greetings);
+  printf("s 9 : |%.*s|\n", biggerPrecision , greetings);
+  printf("s 10 : |%-.*s|\n", biggerPrecision, greetings);
+  printf("s 11 : |%-*s|\n", biggerPrecision , greetings);
+
+  printf("\n\n\n");
+
+  printf("c 1 : |%-8c|\n", greetings[3]);
+  printf("c 2 : |%8c|\n", greetings[3]);
+  printf("c 3 : |%c|\n", greetings[3]);
+  printf("c 4 : |%-c|\n", greetings[3]);
+  printf("c 5 : |%-*c|\n", precision, greetings[3]);
+  printf("c 6 : |%*c|\n", biggerPrecision, greetings[3]);
+  printf("c 7 : |%-*c|\n", biggerPrecision, greetings[3]);
+
+  printf("\n\n\n");
+
+  printf("d 1 : |%.d|\n", enteri);
+  printf("d 2 : |%-d|\n", enteri);
+  printf("d 3 : |%*d|\n", precision, enteri);
+  printf("d 4 : |%0d|\n", enteri);
+  printf("d 5 : |%-.d|\n", enteri);
+  printf("d 6 : |%.*d|\n", precision, enteri);
+  printf("d 7 : |%.0d|\n", enteri);
+  printf("d 8 : |%-.0d|\n", enteri);
+  printf("d 9 : |%-.*d|\n", precision, enteri);
+  printf("d 10 : |%-*d|\n", precision, enteri);
+  printf("d 11 : |%0*d|\n", precision, enteri);
 }
