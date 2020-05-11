f12088f9cad05d16f8883e7af64b4bd5e3135ccf
bentowski <benjb85@hotmail.fr>
2020-05-11 12:43:00 +0200

    fonctionnel

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -1,129 +1,118 @@
-# include "ft_printf.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbaudry <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/11 09:59:48 by bbaudry           #+#    #+#             */
+/*   Updated: 2020/05/11 10:29:58 by bbaudry          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void ft_write(char c, t_flags *flags)
+#include "ft_printf.h"
+
+void	ft_write(char c, t_flags *flags)
 {
-  write(1, &c, 1);
-  flags->printed++;
+	write(1, &c, 1);
+	flags->printed++;
 }
 
-// void ft_constante(t_flags *flags, const char *target, int *x, int opt)
-// {
-//   char test;
-//
-//
-//     if (target[*x] == '*' && opt == 1)
-//       flags->constantep = 1;
-//     else if (target[*x] == '*' && opt == 2)
-//       flags->constantew = 1;
-//     else if (target[*x] >= '1' && target[*x] <= '9')
-//     {
-//       while (target[*x] >= '0' && target[*x] <= '9')
-//       {
-//         write(1, "ok\n", 3);
-//         if (opt == 1)
-//           flags->precision = (flags->precision * 10) + (target[*x] - 48);
-//         else if (opt == 2)
-//           flags->width = (flags->width * 10) + (target[*x] - 48);
-//         x++;
-//       }
-//       x--;
-//     }
-// }
-
-int ft_disturb(int i, const char *target, char *allindexs, t_flags *flags)
+int		ft_constante(t_flags *flags, const char *target, int x, int opt)
 {
-  int y;
-  int x;
-  char test;
+	int	tmp;
 
-  x = i - 1;
-  while (target[++x] && (flags->decalage = x - i) >= 0)
-  {
-    y = -1;
-    while (y++ < 8)
-      if (allindexs[y] == target[x])
-        return (y);
-    if (target[x] == '-')
-      flags->neg = 1;
-    else if (target[x] == '.')
-      flags->cutter = 1;
-    else if (target[x] == '0')
-      flags->zero = 1;
-    else if (flags->cutter == 1)
-    {
-      if (target[x] == '*')
-        flags->constantep += 1;
-      else if (target[x] >= '1' && target[x] <= '9')
-      {
-        while (target[x] >= '0' && target[x] <= '9')
-          flags->precision = (flags->precision * 10) + (target[x++] - 48);
-        x--;
-      }
-    }
-    else if (flags->cutter == 0)
-    {
-      if (target[x] == '*')
-        flags->constantew += 1;
-      else if (target[x] >= '1' && target[x] <= '9')
-      {
-        while (target[x] >= '0' && target[x] <= '9')
-          flags->width = (flags->width * 10) + (target[x++] - 48);
-        x--;
-      }
-    }
-
-    // else if (flags->cutter == 1)
-    // {
-    //   ft_constante(flags, target, &x, 1);
-    // }
-    // else if (flags->cutter == 0)
-    // {
-    //   test = 48 + x;
-    //   write(1, &test, 1);
-    //   write(1, "ok\n", 3);
-    //   ft_constante(flags, target, &x, 2);
-    // }
-  }
-  return (-1);
+	tmp = x;
+	if (target[tmp] == '*' && opt == 1)
+		flags->constantep = 1;
+	else if (target[tmp] == '*' && opt == 2)
+		flags->constantew = 1;
+	else if (target[tmp] >= '1' && target[tmp] <= '9')
+	{
+		while (target[tmp] >= '0' && target[tmp] <= '9')
+		{
+			if (opt == 1)
+				flags->precision = (flags->precision * 10) +
+					(target[tmp++] - 48);
+			else if (opt == 2)
+				flags->width = (flags->width * 10) + (target[tmp++] - 48);
+		}
+		tmp--;
+	}
+	return (tmp);
 }
 
-void ft_init(t_flags *flags)
+int		ft_choose(int i, const char *target, char *allindexs, t_flags *flags)
 {
-  flags->precision = 0;
-  flags->width = 0;
-  flags->neg = 0;
-  flags->zero = 0;
-  flags->cutter = 0;
-  flags->decalage = 0;
-  flags->constantep = 0;
-  flags->constantew = 0;
+	int	y;
+	int	x;
+
+	x = i - 1;
+	while (target[++x] && (flags->decalage = x - i) >= 0)
+	{
+		y = -1;
+		while (y++ < 7)
+			if (allindexs[y] == target[x])
+				return (y);
+		if (target[x] == '-')
+			flags->neg = 1;
+		else if (target[x] == '.')
+			flags->cutter = 1;
+		else if (target[x] == '0')
+			flags->zero = 1;
+		else if (flags->cutter == 1)
+			x = ft_constante(flags, target, x, 1);
+		else if (flags->cutter == 0)
+			x = ft_constante(flags, target, x, 2);
+	}
+	return (-1);
 }
 
-int ft_printf(const char *line, ...)
+void	ft_dispatch(va_list *list_args, t_flags *flags, const char *line)
 {
-  void (*functions[8])(va_list *, t_flags *) = {ft_c, ft_s, ft_di, ft_x, ft_X,
-    ft_u, ft_di, ft_p};
-  char allindexs[9] = {'c', 's', 'd', 'x', 'X', 'u', 'i', 'p', '\0'};
-  va_list list_args;
-  int x;
-  t_flags flags;
+	void	(*functions[8])(va_list *, t_flags *);
+	char	*allindexs;
 
-  x = -1;
-  flags.printed = 0;
-  va_start(list_args, line);
-  while (line[++x])
-  {
-    ft_init(&flags);
-    if (x != 0 && line[x - 1] == '%')
-    {
-      if (line[x] == '%')
-        ft_write(line[x], &flags);
-      (*functions[ft_disturb(x, line, allindexs, &flags)])(&list_args, &flags);
-      while (flags.decalage-- > 0)
-        x++;
-    }
-    else if (line[x] != '%')
-      ft_write(line[x], &flags);
-  }
-  return (flags.printed);
+	allindexs = "csdxXuip";
+	functions[0] = ft_c;
+	functions[1] = ft_s;
+	functions[2] = ft_di;
+	functions[3] = ft_x;
+	functions[4] = ft_xg;
+	functions[5] = ft_u;
+	functions[6] = ft_di;
+	functions[7] = ft_p;
+	if (line[++flags->suivi] != '%')
+		(*functions[ft_choose(flags->suivi, line, allindexs,
+					flags)])(list_args, flags);
+	else
+		ft_write(line[flags->suivi], flags);
+	while (flags->decalage-- > 0)
+		flags->suivi++;
+}
+
+int		ft_printf(const char *line, ...)
+{
+	va_list	list_args;
+	t_flags	flags;
+
+	flags.printed = 0;
+	flags.suivi = -1;
+	va_start(list_args, line);
+	while (line[++flags.suivi])
+	{
+		flags.precision = 0;
+		flags.width = 0;
+		flags.neg = 0;
+		flags.zero = 0;
+		flags.cutter = 0;
+		flags.decalage = 0;
+		flags.constantep = 0;
+		flags.constantew = 0;
+		if (line[flags.suivi] == '%')
+			ft_dispatch(&list_args, &flags, line);
+		else
+			ft_write(line[flags.suivi], &flags);
+	}
+	return (flags.printed);
 }
