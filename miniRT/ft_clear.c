dea704fe36a64be9894329a0659234531a140c3d
benjamin <benjamin.baudry@hotmail.com>
2020-12-07 04:32:09 +0100

    implementation de la nouvelle structure map pour en gerer une seule, gestion des leaks pour celle ci

---
+++ miniRT/ft_clear.c
@@ -0,0 +1,72 @@
+#include "minirt.h"
+
+void ft_clear_base(t_base *base)
+{
+  free(base->origins);
+  free(base->vdir);
+  free(base->color);
+  free(base);
+}
+
+void ft_clear_objs(t_objs *objs)
+{
+  t_objs *ptr;
+  t_objs *tmp;
+
+  ptr = objs;
+  while (ptr)
+  {
+    tmp = ptr->next;
+    if (ptr->p2)
+      free(ptr->p2);
+    if (ptr->p3)
+      free(ptr->p3);
+    if (ptr->base)
+      ft_clear_base(ptr->base);
+    free(ptr);
+    ptr = tmp;
+  }
+}
+
+void ft_clear_cams(t_cams *cams)
+{
+  t_cams *ptr;
+  t_cams *tmp;
+
+  ptr = cams;
+  while (ptr)
+  {
+    tmp = ptr->next;
+    if (ptr->base)
+      ft_clear_base(ptr->base);
+    free(ptr);
+    ptr = tmp;
+  }
+}
+
+void ft_clear_lights(t_lights *lights)
+{
+  t_lights *ptr;
+  t_lights *tmp;
+
+  ptr = lights;
+  while (ptr)
+  {
+    tmp = ptr->next;
+    if (ptr->base)
+      ft_clear_base(ptr->base);
+    free(ptr);
+    ptr = tmp;
+  }
+}
+
+void ft_clear(t_map *map)
+{
+  printf("%s\n", "ok");
+  free(map->ambiant);
+  free(map->resolution);
+  ft_clear_objs(map->objs);
+  ft_clear_cams(map->cams);
+  ft_clear_lights(map->lights);
+  free(map);
+}
--- miniRT/minirt.h
+++ miniRT/minirt.h
@@ -7,67 +7,87 @@
 #ifndef MINIRT_H
 # define MINIRT_H
 
-typedef struct s_vecteur
+typedef struct s_triade
 {
   double x;
   double y;
   double z;
-}              t_vecteur;
+}              t_triade;
 
-typedef struct s_sphere
+typedef struct s_base
 {
-  double x;
-  double y;
-  double z;
-  double r;
-}              t_sphere;
+  t_triade *origins;
+  t_triade *vdir;
+  t_triade *color;
 
-typedef struct s_list
+}               t_base;
+
+typedef struct s_ambiant
 {
-  int type;
-  double x;
-  double y;
-  double z;
-  double x2;
-  double y2;
-  double z2;
-  double x3;
-  double y3;
-  double z3;
-  double vx;
-  double vz;
-  double vy;
-  double puissance;
+  double  ambiant;
+  t_triade *color;
+}               t_ambiant;
+
+typedef struct s_resolution
+{
+  int win_width;
+  int win_height;
+}               t_resolution;
+
+typedef struct s_objs
+{
+  t_base *base;
+  t_triade *p2;
+  t_triade *p3;
   double diameter;
   double height;
-  int fov;
-  int color1;
-  int color2;
-  int color3;
+  struct s_objs *next;
+}               t_objs;
 
-  struct s_list *next;
+typedef struct s_cams
+{
+  t_base *base;
+  double fov;
+  struct s_cams *next;
+}               t_cams;
 
-}             t_list;
+typedef struct s_lights
+{
+  t_base *base;
+  double lumens;
+  struct s_lights *next;
+}               t_lights;
+
+typedef struct s_map
+{
+  t_ambiant     *ambiant;
+  t_resolution  *resolution;
+  t_objs        *objs;
+  t_cams        *cams;
+  t_lights      *lights;
+}             t_map;
 
 // int ft_parse(char **line, int fd, int *win_width, int *win_height);
-int init_sphere(char *line, int i, t_list ****obj);
-int init_plane(char *line, int i, t_list ****obj);
-int init_square(char *line, int i, t_list ****obj);
-int init_cylinder(char *line, int i, t_list ****obj);
-int init_triangle(char *line, int i, t_list ****obj);
-int ft_coordonnees(t_list *new, char *line, int i, int opt);
-int ft_structuration(t_list *new, char *line, int i, int opt);
-void new_init(t_list *new);
+// int init_sphere(char *line, int i, t_list ****obj);
+// int init_plane(char *line, int i, t_list ****obj);
+// int init_square(char *line, int i, t_list ****obj);
+// int init_cylinder(char *line, int i, t_list ****obj);
+// int init_triangle(char *line, int i, t_list ****obj);
+// int ft_triade(t_list *new, char *line, int i, int opt);
+// int ft_structuration(t_list *new, char *line, int i, int opt);
+// void new_init(t_list *new);
 int ft_space(char *line, int i);
-int ft_color(t_list *new, char *line, int i);
+// int ft_color(t_list *new, char *line, int i);
 void	*ft_calloc(size_t count, size_t size);
-double ft_routine(double x, char *line, int *i, int opt);
-int resolution(int *res, char *line, int i);
-int ambiance(char *line, int i, t_list ****obj);
-int camera(char *line, int i, t_list ****obj);
-int light(char *line, int i, t_list ****obj);
-int ft_parse(t_list **obj, char **line, int fd, int *res);
-int foisdeux(char **line, int *ayet, int *ryet);
+// double ft_routine(double x, char *line, int *i, int opt);
+int resolution(t_map *map, char *line, int i);
+// int ambiance(char *line, int i, t_list ****obj);
+// int camera(char *line, int i, t_list ****obj);
+// int light(char *line, int i, t_list ****obj);
+int ft_parse(t_map *map, char *givedmap);
+int foisdeux(char *line, int *ayet, int *ryet);
+void ft_clear(t_map *map);
+
 
 
 
