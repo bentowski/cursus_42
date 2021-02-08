6be60af8c6e77878d3e370763cdfba34dc294cbe
benjamin <benjamin.baudry@hotmail.com>
2021-02-08 02:08:27 +0100

    ajout sommaire des ombres, pb si on croise un objet apres la lumiere

--- miniRT/srcs/coloring.c
+++ miniRT/srcs/coloring.c
@@ -1,29 +1,32 @@
 #include "../minirt.h"
 
-int get_rgb(int color1, int color2, double intensity, double lightning)
+int get_rgb(int color1, int color2, double intensity, double light)
 {
   int ret;
 
-  ret = (color1 - (255 - color2)) * lightning;
+  ret = (color1 - (255 - color2)) * light;
   ret += (color1 - (255 - color2)) * intensity;
   if (ret > 255)
-    ret = 255;
+    return (255);
   if (ret < 0)
-    ret = 0;
+    return (0);
   return (ret);
 }
 
 unsigned long int get_color(t_ambiant *ambiant, t_objs *ptr, double intensity)
 {
-  int color;
+  t_triade color1;
+  t_triade color2;
+  double light;
   unsigned long int ret;
 
   if (!ptr)
     return (0);
-  color = get_rgb(ambiant->color->x, ptr->base->color->x, intensity, ambiant->lumens);
-  ret = color * 256 * 256;
-  color = get_rgb(ambiant->color->y, ptr->base->color->y, intensity, ambiant->lumens);
-  ret += color * 256;
-  color = get_rgb(ambiant->color->z, ptr->base->color->z, intensity, ambiant->lumens);
-  return (ret + color);
+  color1 = *ambiant->color;
+  color2 = *ptr->base->color;
+  light = ambiant->lumens;
+  ret = get_rgb(color1.x, color2.x, intensity, light) * 256 * 256;
+  ret += get_rgb(color1.y, color2.y, intensity, light) * 256;
+  ret += get_rgb(color1.z, color2.z, intensity, light);
+  return (ret);
 }
--- miniRT/srcs/raytracing.c
+++ miniRT/srcs/raytracing.c
@@ -1,6 +1,25 @@
 #include "../minirt.h"
 
-double get_intensity(t_map *map, t_triade ldir, t_triade n)
+int get_shadows(t_map *map, t_triade ray, t_triade *p)
+{
+  t_objs *ptr;
+  double alpha;
+
+  ptr = map->objs;
+  while (ptr->next)
+  {
+    if (ptr->type == 3)
+      if ((alpha = intersect_plan(ray, ptr, p)) >= 0)
+        return (0);
+    if (ptr->type == 1)
+      if ((alpha = intersect_sphere(ray, ptr, p)) >= 0)
+        return (0);
+    ptr = ptr->next;
+  }
+  return (1);
+}
+
+double get_intensity(t_map *map, t_triade ldir, t_triade n, t_triade position)
 {
   double ldist;
   double ret;
@@ -11,7 +30,10 @@ double get_intensity(t_map *map, t_triade ldir, t_triade n)
     ret = ((map->lights->lumens) * scale(&ldir, &n)) / ldist;
   if (ret < 0)
     ret = 0;
-  return (ret);
+  position.x += n.x * 0.001;
+  position.y += n.y * 0.001;
+  position.z += n.z * 0.001;
+  return (ret * get_shadows(map, ldir, &position));
 }
 
 unsigned long int ft_raytracing(t_map *map, t_triade ray)
@@ -26,7 +48,9 @@ unsigned long int ft_raytracing(t_map *map, t_triade ray)
   t = -1;
   target = intersect(map->objs, map->cams->base->origins, ray, &t);
   position = get_position(map->cams->base->origins, ray, t);
-  intensity = get_intensity(map, get_norme(vector(map->lights->base->origins, &position)), get_normale(target, position, map));
+  intensity = get_intensity(map,
+    get_norme(vector(map->lights->base->origins, &position)),
+    get_normale(target, position, map), position);
   ret = get_color(map->ambiant, target, intensity);
   return (ret);
 }
